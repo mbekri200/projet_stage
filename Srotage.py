534d8ca58ea8062e2491de3ccbd5fe5a829fa560
mbekri200 <merybekri5@gmail.com>
2023-05-01 15:15:45 +0000

    Pending changes exported from your codespace

---
+++ Srotage.py
@@ -0,0 +1,17 @@
+import os, uuid, sys
+from azure.identity import DefaultAzureCredential
+from azure.storage.filedatalake import DataLakeServiceClient
+from azure.core._match_conditions import MatchConditions
+from azure.storage.filedatalake._models import ContentSettings
+
+
+def initialize_storage_account(storage_account_name, storage_account_key):
+    
+    try:  
+        global service_client
+
+        service_client = DataLakeServiceClient(account_url="{}://{}.dfs.core.windows.net".format(
+            "https", storage_account_name), credential=storage_account_key)
+    
+    except Exception as e:
+        print(e)
